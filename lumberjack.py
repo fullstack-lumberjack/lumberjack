a3fd4255407cb1fb2bb0de8c8376dade0f6b4fdf
Lance <lanceyvang@gmail.com>
2019-09-17 14:13:14 -0400

    removed hello world

--- lumberjack.py
+++ lumberjack.py
@@ -19,7 +19,6 @@ def main():
     print(switcher.get(sys.argv[2], "invalid log"))
 
 if __name__ == '__main__':
-    print('hello world')
     if len(sys.argv) <= 1:
         print("How to use: lumberjack.py <logfile> <logtype>. Ex: lumberjack.py log.txt -a")
     else: main()
