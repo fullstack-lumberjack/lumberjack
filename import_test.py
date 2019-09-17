3bb03b2b88974703ade8ceb4a764dc46a4a69c51
plamengj <plamen.gj@gmail.com>
2019-09-17 11:27:32 -0400

    initial work

---
+++ import_test.py
@@ -0,0 +1,17 @@
+
+
+class Haley:
+    def __init__ (self, name, age):
+        self.name = name
+        self.age = age
+    def import_plamen():
+        print('our import works')
+
+class Lance:
+    def __init__ (self, name, age):
+        self.name = name
+        self.age = age
+    def import_plamen():
+        print('our import Lance')
+
+
