c7373b3512e84acaaae89c1c2305938df9a4dbe8
lanceyvang <34265760+lanceyvang@users.noreply.github.com>
2019-09-18 17:42:29 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 chmod +x lumberjack.py
-ln -s ./lumberjack.py /usr/local/bin/lumberjack
+ln -s lumberjack.py /usr/local/bin/lumberjack
 sudo apt-get install python3 pip
 sudo pip install colorama
