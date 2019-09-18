9fc6676bb3fd8c9289142dddcd436fa806792b5b
Lance <lanceyvang@gmail.com>
2019-09-18 10:19:08 -0400

    fixed comflicts

--- apache.py
+++ apache.py
@@ -1,34 +1,9 @@
 import sys
-import re
+import utility
 
-def common():
-    content = open(sys.argv[1], 'r').read()
-    all_ips = re.findall(r'[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+', content)
+def main():
+    if sys.argv[2] == True: print('pokemon')
+    else: print('digimon')
 
-    ip_dictionary = {}
-
-    ip_set = set()
-
-    for ip in all_ips:
-        ip_set.add(ip)
-        if ip in ip_dictionary: ip_dictionary[ip] += 1
-        else: ip_dictionary[ip] = 1 
-
-    print(f'There are {len(ip_set)} unique ips.')
-
-    def compare_value(ip):
-        return ip_dictionary[ip]
-
-    sorted_ip_list = sorted(ip_dictionary, key=compare_value, reverse=False)
-
-    print('\nThese are the top 5 least occurring ips:')
-    for ip in sorted_ip_list[:5]:
-        print(f'ip: {ip}, occurrence: {ip_dictionary[ip]}')
-
-    reverse_sorted = sorted(ip_dictionary, key=compare_value, reverse=True)
-
-    print('\nThese are the top 5 most occurring ips:')
-    for ip in reverse_sorted[:5]:
-        print(f'ip: {ip}, occurrence: {ip_dictionary[ip]}')
-
-    return 'END'
+if __name__ == "__main__":
+    main()
--- lumberjack.py
+++ lumberjack.py
@@ -4,9 +4,11 @@ import sys
 import utility
 
 def main():
-    print(utility.common())
+    print(utility.unique_ips())
+    print(utility.most_ips())
+    print(utility.least_ips())
 
 if __name__ == '__main__':
     if len(sys.argv) <= 1:
-        print("How to use: lumberjack.py <logfile> <logtype>. Ex: lumberjack.py log.txt -a")
+        print("How to use: lumberjack.py <logfile> <logtype>. Ex: lumberjack.py log.txt <-a>")
     else: main()
