847e9c42011d9d43b31bd07ae6b822a63021c24f
plamengj <plamen.gj@gmail.com>
2019-10-09 19:21:14 -0400

    almost

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 chmod +x lumberjack.py
 sudo ln -s lumberjack.py /usr/local/bin/lumberjack
 sudo apt-get install python3-pip
-sudo pip3 install colorama
+sudo pip3 install colorama PyInquirer prettytable
